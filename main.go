03a4c7347225c9e2ed8fb53e02cabfa30b799448
snowlab <snowlab@hotmail.com>
2018-10-06 15:36:21 +0800

    配置使用绝对路径

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 	"time"
 )
 
@@ -65,7 +66,13 @@ func getSorts(writer http.ResponseWriter, request *http.Request) {
 }
 
 func loadConfig() {
-	file, err := os.Open("config.json")
+	currentDir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		log.Fatal(err)
+	}
+	configPath := filepath.Join(currentDir, "config.json")
+	fmt.Printf("配置文件：%s", currentDir)
+	file, err := os.Open(configPath)
 	if err != nil {
 		log.Fatalln("Cannot open config file", err)
 	}
@@ -78,11 +85,6 @@ func loadConfig() {
 }
 
 func main() {
-	fmt.Println("本项目属于技术交流，不承担任何版权责任")
-	fmt.Println("不使用时关闭本窗口释放多余的性能开销")
-	fmt.Println("巨应工作室 qq交流群: 641405255")
-	fmt.Println("源码地址：https://github.com/MscoderStudio/LiveWallpaperServer")
-	fmt.Println("《巨应动态壁纸》 第三方数据解析器运行中....")
 	loadConfig()
 	mux := http.NewServeMux()
 	mux.HandleFunc("/tags", getTags)
@@ -96,4 +98,9 @@ func main() {
 		MaxHeaderBytes: 1 << 20,
 	}
 	server.ListenAndServe()
+	fmt.Println("本项目属于技术交流，不承担任何版权责任")
+	fmt.Println("不使用时关闭本窗口释放多余的性能开销")
+	fmt.Println("巨应工作室 qq交流群: 641405255")
+	fmt.Println("源码地址：https://github.com/MscoderStudio/LiveWallpaperServer")
+	fmt.Println("《巨应动态壁纸》 第三方数据解析器运行中....")
 }
